13b11c61b8a4e6919f3e0b3117de2fe3a5d5d4fe
kaoru-kk <kkaoru1998@gmail.com>
2020-10-29 14:27:15 +0900

    リファクタしてログ出力できるように

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const http = require('http');
-const os = require('os');
+const hostname = '127.0.0.1';
+const port = '3333'
 
-http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  const host = os.hostname()
-  res.end(`Hello World! ${host}`)
-}).listen(8989)
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type' , 'text/plain');
+  res.end(`Hello World! kaoru!`)
+})
+
+server.listen(port, hostname, () =>{
+  console.log(`Server running at http://${hostname}:${port}/`)
+});
